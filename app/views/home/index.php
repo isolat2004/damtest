e2e35c51b5e50ac7e4dcddfc845c43ba5cea3e07
franck milandou <isolat74@gmail.com>
2017-04-15 17:38:08 +0200

    la modif fichier index

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -10,7 +10,7 @@
   <body>
     <nav class="navbar navbar-dark bg-success">
       <div class="container">
-        <a href="" class="navbar-brand">Franck dbsff ++++ en modiffffff</a>
+        <a href="" class="navbar-brand">Franck dbsff ++++ en modiffffff blalfdghhrhgyu</a>
       </div>
     </nav>
     <div class="container">
